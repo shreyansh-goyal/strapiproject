d0a01b82b992dc6c93b130929b99972d6f83b119
duckie <sarthakmisraa@gmail.com>
2020-02-18 17:07:34 +0530

    nice

--- run-setup.sh
+++
@@ -1,7 +0,0 @@
-cd /root/tmp/
-git clone git@github.com:not-duckie/strapiproject.git
-npm i strapi
-npm install
-cd /root/nice/server_side/
-strapi build
-strapi start
--- setup.sh
+++
@@ -1,1 +0,0 @@
-cat dbdump/*.sql | mysql -h 127.0.0.1 -u root -ppasswordcomplex -P3308 teacherdb

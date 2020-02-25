bf3613d8733cdd243993fbf4ad6324747e638de2
duckie <40424574+not-duckie@users.noreply.github.com>
2020-02-25 15:52:05 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-git clone https://github.com/not-duckie/strapiproject.git /root/
+git clone https://github.com/not-duckie/strapiproject.git /root/strapiproject
 cd /root/strapiproject/
+npm install
 /etc/init.d/mysql start
 echo "create database teacher;" | mysql -u root --password=M@y@nk250199
 cd server_side
+npm install
 strapi build
 strapi start

a2b6fa3230bb438ba925554b66a48ee863b18d68
duckie <40424574+not-duckie@users.noreply.github.com>
2020-03-09 22:26:05 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ npm install
 /etc/init.d/mysql start
 echo "use mysql" | mysql -u root
 echo 'update user set password=PASSWORD("M@y@nk250199") where User="root";' | mysql -u root mysql
-echo 'update user set plugin="mysql_native_password";' | mysql -u root mysql
+echo 'update user set plugin="mysql_native_password";' | mysql -u root --password=M@y@nk250199 mysql
 /etc/init.d/mysql stop
 /etc/init.d/mysql start
 echo "create database teacher;" | mysql -u root --password=M@y@nk250199

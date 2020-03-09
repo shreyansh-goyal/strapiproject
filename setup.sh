86b7771decc1d62822b7a1ec066ace6d19aa400b
duckie <40424574+not-duckie@users.noreply.github.com>
2020-03-09 22:37:39 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/not-duckie/strapiproject.git /root/strapiproject
 cd /root/strapiproject/
 npm install
+npm i strapi -g
 /etc/init.d/mysql start
 echo "use mysql" | mysql -u root
 echo 'update user set password=PASSWORD("M@y@nk250199") where User="root";' | mysql -u root mysql

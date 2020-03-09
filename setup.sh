ff7e807bbc81a0f95dfc9ff25b1940c5cc48978a
duckie <40424574+not-duckie@users.noreply.github.com>
2020-03-09 22:20:05 +0530

    i hope this works

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@ git clone https://github.com/not-duckie/strapiproject.git /root/strapiproject
 cd /root/strapiproject/
 npm install
 /etc/init.d/mysql start
+echo "use mysql" | mysql -u root
+echo 'update user set password=PASSWORD("M@y@nk250199") where User="root";' | mysql -u root mysql
+echo 'update user set plugin="mysql_native_password";' | mysql -u root mysql
+/etc/init.d/mysql stop
+/etc/init.d/mysql start
 echo "create database teacher;" | mysql -u root --password=M@y@nk250199
 cd server_side
-npm install strapi -g
 npm install
 strapi build
+strapi start

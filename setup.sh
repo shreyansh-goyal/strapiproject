ffc021ba78d150f0e35bbf802b1b47c5af265b7d
duckie <sarthakmisraa@gmail.com>
2020-06-07 13:59:14 +0530

    this will work

--- setup.sh
+++ setup.sh
@@ -4,12 +4,11 @@ npm install
 npm i strapi -g
 /etc/init.d/mysql start
 echo "use mysql" | mysql -u root
-#echo 'update user set password=PASSWORD("shreyanshgoyal") where User="admin";' | mysql -u root mysql
 echo "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'shreyanshgoyal';" | mysql -u root
-#echo 'update user set plugin="mysql_native_password";' | mysql -u root --password=shreyanshgoyal mysql
+echo 'update user set password=PASSWORD("shreyanshgoyal") where User="admin";' | mysql -u root mysql
+echo 'update user set plugin="mysql_native_password";' | mysql -u admin --password=shreyanshgoyal mysql
 /etc/init.d/mysql stop
 /etc/init.d/mysql start
-echo "create database teacher;" | mysql -u root --password=M@y@nk250199
-cd server_side
+echo "create database strapi;" | mysql -u admin --password=shreyanshgoyal
 npm install
 strapi build
